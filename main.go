da1e9a70981a6c74669997d59a79783e5093996a
akmalsan <15670753+akmalsan@users.noreply.github.com>
2023-01-09 21:23:23 +0700

    [docs] add documentation for home and contact route handlers

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+// homeHandler handles the home route and returns the home page
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
 }
 
+// contactHandler handles the contact route and returns the contact page
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1>")
